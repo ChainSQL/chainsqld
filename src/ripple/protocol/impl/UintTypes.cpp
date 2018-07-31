b56b0630f5f0e6d43e8f0671fda8ff6106731f11
luleigreat <luleigreat@163.com>
2018-07-31 11:34:08 +0800

    change coin length limit to 10

--- src/ripple/protocol/impl/UintTypes.cpp
+++ src/ripple/protocol/impl/UintTypes.cpp
@@ -27,74 +27,78 @@ namespace ripple {
 
 std::string to_string(Currency const& currency)
 {
-    // Characters we are willing to allow in the ASCII representation of a
-    // three-letter currency code.
-    static std::string const allowed_characters =
-        "abcdefghijklmnopqrstuvwxyz"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "0123456789"
-        "<>(){}[]|?!@#$%^&*";
+	// Characters we are willing to allow in the ASCII representation of a
+	// three-letter currency code.
+	static std::string const allowed_characters =
+		"abcdefghijklmnopqrstuvwxyz"
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		"0123456789"
+		"<>(){}[]|?!@#$%^&*";
 
-    if (currency == zero)
-        return systemCurrencyCode();
+	if (currency == zero)
+		return systemCurrencyCode();
 
-    if (currency == noCurrency())
-        return "1";
+	if (currency == noCurrency())
+		return "1";
 
-    static Currency const sIsoBits (
-        from_hex_text<Currency>("FFFFFFFFFFFFFFFFFFFFFFFF000000FFFFFFFFFF"));
+	static Currency const sIsoBits(
+		from_hex_text<Currency>("FFFFFFFFFFFFFFFFFFFFFF000000000000000000"));
 
-    if ((currency & sIsoBits).isZero ())
-    {
-        // The offset of the 3 character ISO code in the currency descriptor
-        int const isoOffset = 12;
+	if ((currency & sIsoBits).isZero())
+	{
+		// The offset of the 3 character ISO code in the currency descriptor
+		int const isoOffset = 10;
+		int const startOffset = 9;
 
-        std::string const iso(
-            currency.data () + isoOffset,
-            currency.data () + isoOffset + 3);
+		char length = currency.data()[startOffset];
 
-        // Specifying the system currency code using ISO-style representation
-        // is not allowed.
-        if ((iso != systemCurrencyCode()) &&
-            (iso.find_first_not_of (allowed_characters) == std::string::npos))
-        {
-            return iso;
-        }
-    }
+		std::string const iso(
+			currency.data() + isoOffset,
+			currency.data() + isoOffset + length);
 
-    return strHex (currency.begin (), currency.size ());
+		// Specifying the system currency code using ISO-style representation
+		// is not allowed.
+		if ((iso != systemCurrencyCode()) &&
+			(iso.find_first_not_of(allowed_characters) == std::string::npos))
+		{
+			return iso;
+		}
+	}
+
+	return strHex(currency.begin(), currency.size());
 }
 
 bool to_currency(Currency& currency, std::string const& code)
 {
-    if (code.empty () || !code.compare (systemCurrencyCode()))
-    {
-        currency = zero;
-        return true;
-    }
+	if (code.empty() || !code.compare(systemCurrencyCode()))
+	{
+		currency = zero;
+		return true;
+	}
 
-    static const int CURRENCY_CODE_LENGTH = 3;
-    if (code.size () == CURRENCY_CODE_LENGTH)
-    {
-        Blob codeBlob (CURRENCY_CODE_LENGTH);
+	static const int CURRENCY_CODE_LENGTH = 3;
+	static const int CURRENCY_CODE_MAXLENGTH = 10;
+	if (code.size() >= CURRENCY_CODE_LENGTH && code.size() <= CURRENCY_CODE_MAXLENGTH)
+	{
+		Blob codeBlob(code.size());
 
-        std::transform (code.begin (), code.end (), codeBlob.begin (), ::toupper);
+		std::transform(code.begin(), code.end(), codeBlob.begin(), ::toupper);
 
-        Serializer  s;
+		Serializer  s;
 
-        s.addZeros (96 / 8);
-        s.addRaw (codeBlob);
-        s.addZeros (16 / 8);
-        s.addZeros (24 / 8);
+		s.addZeros(72 / 8);
+		s.add8(code.size());
+		s.addRaw(codeBlob);
+		s.addZeros(20 - 10 - code.size());
 
-        s.get160 (currency, 0);
-        return true;
-    }
+		s.get160(currency, 0);
+		return true;
+	}
 
-    if (40 == code.size ())
-        return currency.SetHex (code);
+	if (40 == code.size())
+		return currency.SetHex(code);
 
-    return false;
+	return false;
 }
 
 Currency to_currency(std::string const& code)
