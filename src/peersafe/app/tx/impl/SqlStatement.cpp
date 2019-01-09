62dafa70a561b45ce096d99227e6533175d00592
luleigreat <luleigreat@163.com>
2019-01-09 19:59:44 +0800

    fix bug 'dump when insert an operation-rule table'

--- src/peersafe/app/tx/impl/SqlStatement.cpp
+++ src/peersafe/app/tx/impl/SqlStatement.cpp
@@ -266,6 +266,11 @@ namespace ripple {
 		if(!OperationRule::hasOperationRule(view,tx))
 			return tesSUCCESS;
 		//
+		if (ctx_.app.getTxStoreDBConn().GetDBConn() == nullptr ||
+			ctx_.app.getTxStoreDBConn().GetDBConn()->getSession().get_backend() == nullptr)
+		{
+			return tefDBNOTCONFIGURED;
+		}
 		auto envPair = getTransactionDBEnv(ctx_);
 		TxStoreTransaction stTran(envPair.first);
 		TxStore& txStore = *envPair.second;
