31eb85190069174a8d43eb75c6e555239f2e6919
halberdholder <825656416@qq.com>
2019-07-30 19:43:32 +0800

    add intel_tbb project for parallel apply tx, Build success with VS2015

---
+++ src/peersafe/app/tx/ParallelApply.h
@@ -0,0 +1,52 @@
+#ifndef CHAINSQL_APP_TX_PARALLELAPPLY_H_INCLUDED
+#define CHAINSQL_APP_TX_PARALLELAPPLY_H_INCLUDED
+
+#include <ripple/app/main/Application.h>
+#include <ripple/beast/utility/Journal.h>
+#include <ripple/protocol/STTx.h>
+#include <ripple/ledger/ApplyView.h>
+#include <tbb/blocked_range.h>
+#include <tbb/concurrent_vector.h>
+
+namespace ripple {
+
+class ParallelApply
+{
+public:
+    using Txs = tbb::concurrent_vector<std::shared_ptr<STTx const>>;
+
+    ParallelApply(
+        Txs &a, Txs &r, bool &certainRetry, int &changes, 
+        Application& app, OpenView& view, beast::Journal j
+    )
+        : shouldApplyTxs_(a)
+        , retryTxs_(r)
+        , certainRetry_(certainRetry)
+        , changes_(changes)
+        , app_(app)
+        , view_(view)
+        , j(j)
+    {}
+
+    ~ParallelApply() {}
+
+    void operator() (Txs::range_type &r) const;
+
+    void preRetry();
+
+private:
+    bool &certainRetry_;
+    int &changes_;
+
+    Txs &shouldApplyTxs_;
+    Txs &retryTxs_;
+
+    Application& app_;
+    OpenView& view_;
+    beast::Journal j;
+
+};
+
+}
+
+#endif
---
+++ src/peersafe/app/tx/impl/ParallelApply.cpp
@@ -0,0 +1,50 @@
+
+#include <peersafe/app/tx/ParallelApply.h>
+#include <ripple/app/tx/apply.h>
+
+
+namespace ripple {
+
+void ParallelApply::operator() (Txs::range_type &r) const
+{
+    for (auto it = r.begin(); it != r.end(); ++it)
+    {
+        try
+        {
+            switch (applyTransaction(
+                app_, view_, *it->get(), certainRetry_, tapNO_CHECK_SIGN | tapForConsensus, j))
+            {
+            case ApplyResult::Success:
+                ++changes_;
+                break;
+
+            case ApplyResult::Fail:
+                break;
+
+            case ApplyResult::Retry:
+                retryTxs_.push_back(*it);
+                ++it;
+                break;
+            }
+        }
+        catch (std::exception const&)
+        {
+            JLOG(j.warn()) << "Transaction throws";
+            it = retryTxs_.push_back(*it);
+        }
+    }
+}
+
+void ParallelApply::preRetry()
+{
+    // Clear already applied txs.
+    shouldApplyTxs_.clear();
+
+    // Then swap should retry txs to shouldApply, prepare for next apply round.
+    // Also clear should retry txs.
+    shouldApplyTxs_.swap(retryTxs_);
+
+    changes_ = 0;
+}
+
+}
