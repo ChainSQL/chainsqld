532a647f94b9f7617c91989314b9e5e38d92c9b8
cruanEzhou <cruanysu@gmail.com>
2020-01-07 15:39:05 +0800

    fixed windows unity compile error

--- src/peersafe/crypto/impl/X509.cpp
+++ src/peersafe/crypto/impl/X509.cpp
@@ -27,6 +27,7 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #include <openssl/pem.h>
 #include <openssl/x509_vfy.h>
 #include <openssl/ec.h>
+
 #include <ripple/crypto/impl/openssl.h>
 #include <ripple/crypto/GenerateDeterministicKey.h>
 #include <peersafe/crypto/X509.h>
@@ -238,8 +239,8 @@ namespace ripple {
 		//int               eccgrp;
 		int             ret = 0;
 		int             nVersion = 1;
-		X509_REQ        *x509_req = NULL;
-		X509_NAME       *x509_name = NULL;
+		X509_REQ        *x509_req  = NULL;
+		X509_name_st    *x509_name = NULL;
 	//	RSA             *tem = NULL;
 		BIO             *out = NULL;
 
