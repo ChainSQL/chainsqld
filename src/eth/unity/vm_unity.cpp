0c0dd7eefec0390d9d30a7ebcda53c53a66f14ed
Mojicode <lascion@163.com>
2020-03-30 10:51:13 +0800

    add vm_unity.cpp to support unity compile

---
+++ src/eth/unity/vm_unity.cpp
@@ -0,0 +1,6 @@
+#include <eth/vm/VMC.cpp>
+#include <eth/vm/ExtVMFace.cpp>
+#include <eth/vm/VMFactory.cpp>
+#include <eth/vm/executor/interpreter/VM.cpp>
+#include <eth/vm/executor/interpreter/VMCalls.cpp>
+#include <eth/vm/executor/interpreter/VMOpt.cpp>
