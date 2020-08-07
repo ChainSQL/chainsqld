3214e6bd1aa6ff85af4c9433b576dcb6e6026a5f
Mojicode <lascion@163.com>
2020-08-07 14:53:05 +0800

    update cmake file and unity code

--- src/peersafe/unity/gm_encrypt.cpp
+++ src/peersafe/unity/gm_encrypt.cpp
@@ -1,10 +1,11 @@
 #include <BeastConfig.h>
 
-#include <peersafe/gmencrypt/GmEncrypt.cpp>
-#include <peersafe/gmencrypt/GmEncryptObj.cpp>
+#include <peersafe/gmencrypt/impl/GmEncrypt.cpp>
+#include <peersafe/gmencrypt/impl/GmEncryptObj.cpp>
 #include <peersafe/gmencrypt/hardencrypt/impl/sdkey.cpp>
 #include <peersafe/gmencrypt/hardencrypt/impl/sjkCard.cpp>
-#include <peersafe/gmencrypt/GmCheck.cpp>
+#include <peersafe/gmencrypt/softencrypt/impl/GmSoftEncrypt.cpp>
+#include <peersafe/gmencrypt/impl/GmCheck.cpp>
 #include <peersafe/gmencrypt/randomcheck/impl/randCheck.cpp>
 #include <peersafe/gmencrypt/randomcheck/impl/log.c>
 #include <peersafe/gmencrypt/randomcheck/impl/cephes.c>
