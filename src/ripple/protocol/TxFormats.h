3352200f68254ea2262a83180cb66b7ae3b079e6
luleigreat <luleigreat@163.com>
2021-01-14 17:11:38 +0800

    adjust transactiontype, ttACCOUNT_DELETE to 51

--- src/ripple/protocol/TxFormats.h
+++ src/ripple/protocol/TxFormats.h
@@ -30,44 +30,42 @@ namespace ripple {
 
     @ingroup protocol
 */
-enum TxType
-{
-    ttINVALID           = -1,
+enum TxType {
+    ttINVALID = -1,
 
-    ttPAYMENT           = 0,
-    ttESCROW_CREATE     = 1,
-    ttESCROW_FINISH     = 2,
-    ttACCOUNT_SET       = 3,
-    ttESCROW_CANCEL     = 4,
-    ttREGULAR_KEY_SET   = 5,
-    ttNICKNAME_SET      = 6, // open
-    ttOFFER_CREATE      = 7,
-    ttOFFER_CANCEL      = 8,
-    no_longer_used      = 9,
-    ttTICKET_CREATE     = 10,
-    ttTICKET_CANCEL     = 11,
-    ttSIGNER_LIST_SET   = 12,
-    ttPAYCHAN_CREATE    = 13,
-    ttPAYCHAN_FUND      = 14,
-    ttPAYCHAN_CLAIM     = 15,
-    ttCHECK_CREATE       =  16,
-    ttCHECK_CASH         =  17,
-    ttCHECK_CANCEL       =  18,
-    ttDEPOSIT_PREAUTH    =  19,
-    ttTRUST_SET         = 20,
-    ttACCOUNT_DELETE    = 21,
+    ttPAYMENT = 0,
+    ttESCROW_CREATE = 1,
+    ttESCROW_FINISH = 2,
+    ttACCOUNT_SET = 3,
+    ttESCROW_CANCEL = 4,
+    ttREGULAR_KEY_SET = 5,
+    ttNICKNAME_SET = 6,  // open
+    ttOFFER_CREATE = 7,
+    ttOFFER_CANCEL = 8,
+    no_longer_used = 9,
+    ttTICKET_CREATE = 10,
+    ttTICKET_CANCEL = 11,
+    ttSIGNER_LIST_SET = 12,
+    ttPAYCHAN_CREATE = 13,
+    ttPAYCHAN_FUND = 14,
+    ttPAYCHAN_CLAIM = 15,
+    ttCHECK_CREATE = 16,
+    ttCHECK_CASH = 17,
+    ttCHECK_CANCEL = 18,
+    ttDEPOSIT_PREAUTH = 19,
+    ttTRUST_SET = 20,
 
+    ttTABLELISTSET = 21,
+    ttSQLSTATEMENT = 22,
+    ttSQLTRANSACTION = 23,
+    ttCONTRACT = 24,
+    ttSCHEMA_CREATE = 25,
+    ttSCHEMA_MODIFY = 26,
 
-
-    ttTABLELISTSET      = 22,
-    ttSQLSTATEMENT      = 23,
-    ttSQLTRANSACTION    = 24,
-	ttCONTRACT			= 25,
-	ttSCHEMA_CREATE		= 26,
-	ttSCHEMA_MODIFY		= 27,
-    ttAMENDMENT         = 100,
-    ttFEE               = 101,
-    ttUNL_MODIFY        = 102,
+    ttACCOUNT_DELETE = 51,
+    ttAMENDMENT = 100,
+    ttFEE = 101,
+    ttUNL_MODIFY = 102,
 };
 
 /** Manages the list of known transaction formats.
