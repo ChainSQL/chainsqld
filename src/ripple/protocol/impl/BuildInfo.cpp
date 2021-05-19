6e8da491effa3831ec849e94022bd6b386c80f1a
luleigreat <luleigreat@163.com>
2021-05-19 15:48:52 +0800

    upgrade version to 1.1.5-pop

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -33,7 +33,7 @@ char const* const versionString =
     //  The build version number. You must edit this for each release
     //  and follow the format described at http://semver.org/
     //
-        "1.1.4-pop"
+        "1.1.5-pop"
 
 #if defined(DEBUG) || defined(SANITIZER)
        "+"
