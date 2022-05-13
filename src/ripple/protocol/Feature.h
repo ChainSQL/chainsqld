4ebb45c1bd0378a5385be4f9b2c5abac82e047a9
luleigreat <luleigreat@163.com>
2022-05-13 18:03:00 +0800

    Fix merge error

--- src/ripple/protocol/Feature.h
+++ src/ripple/protocol/Feature.h
@@ -394,6 +394,8 @@ extern uint256 const fixAmendmentMajorityCalc;
 extern uint256 const featureNegativeUNL;
 extern uint256 const featureTableSleChange;
 extern uint256 const featureContractStorage;
+extern uint256 const featureTableGrant;
+extern uint256 const featurePromethSLEHideInMeta;
 
 }  // namespace ripple
 
