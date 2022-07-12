4db80537e3bf53e7fcc6f09ff6428270cede7994
luleigreat <luleigreat@163.com>
2022-07-12 15:09:46 +0800

    Add file ripemd160 to compile

--- Builds/CMake/ChainsqldCore.cmake
+++ Builds/CMake/ChainsqldCore.cmake
@@ -731,6 +731,7 @@ target_sources (chainsqld PRIVATE
   src/peersafe/precompiled/TableOpPrecompiled.cpp
   src/peersafe/precompiled/ToolsPrecompiled.cpp
   src/peersafe/precompiled/Utils.cpp
+  src/peersafe/precompiled/ripemd160.cpp
   src/peersafe/protocol/impl/Contract.cpp
   src/peersafe/protocol/impl/STEntry.cpp
   src/peersafe/protocol/impl/STMap256.cpp
