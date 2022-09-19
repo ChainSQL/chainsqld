81ab7441f997a1f1e5710df0414d38b039fa7ad3
luleigreat <luleigreat@163.com>
2022-09-19 09:47:58 +0800

    Use soci-chainsql with reconnect when error

--- Builds/CMake/deps/Soci.cmake
+++ Builds/CMake/deps/Soci.cmake
@@ -42,7 +42,7 @@ else()
   ExternalProject_Add (soci
     PREFIX ${nih_cache_path}
     GIT_REPOSITORY https://github.com/ChainSQL/soci.git
-    GIT_TAG chainsql
+    GIT_TAG c6e581399415d6bc366a83532e85ae64e0204ef4
     GIT_SHALLOW ON
     # We had an issue with soci integer range checking for boost::optional
     # and needed to remove the exception that SOCI throws in this case.
