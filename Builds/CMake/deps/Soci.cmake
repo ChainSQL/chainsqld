6b451295c4459ba6ece1d17c03a217ee6ecc233e
luleigreat <luleigreat@163.com>
2022-09-19 09:51:32 +0800

    Use different tag with develop for soci,make conflict when merge

--- Builds/CMake/deps/Soci.cmake
+++ Builds/CMake/deps/Soci.cmake
@@ -42,7 +42,7 @@ else()
   ExternalProject_Add (soci
     PREFIX ${nih_cache_path}
     GIT_REPOSITORY https://github.com/ChainSQL/soci.git
-    GIT_TAG chainsql
+    GIT_TAG	b29dd5f6c082f14b7e88adb8955ef99a1a866427
     GIT_SHALLOW ON
     # We had an issue with soci integer range checking for boost::optional
     # and needed to remove the exception that SOCI throws in this case.
