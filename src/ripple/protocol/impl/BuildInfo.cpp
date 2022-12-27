739f5be9166ae5995ffb42f4d2901c346d18c73e
luleigreat <luleigreat@163.com>
2022-12-27 10:52:07 +0800

    Version number to 3.4.3

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -33,7 +33,7 @@ namespace BuildInfo {
 //  and follow the format described at http://semver.org/
 //------------------------------------------------------------------------------
 // clang-format off
-char const* const versionString = "3.4.1"
+char const* const versionString = "3.4.3"
 // clang-format on
 
 #if defined(DEBUG) || defined(SANITIZER)
